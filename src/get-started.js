009fcea7d12b55c69018b82612e39d16dfeb4523
vitorlfreitas <vitor.freitas@student.griffith.ie>
2024-07-22 17:42:06 +0100

    Add header responsivo

--- src/get-started.js
+++ src/get-started.js
@@ -1,25 +1,53 @@
 const button = document.querySelector(".page-header__link");
-
-const form = document.querySelector(".form-container");
+const formContainer = document.querySelector(".form-container");
 const formPopUp = document.querySelector(".form-popup");
 const overlay = document.querySelector('.overlay');
 const xClose = document.querySelector('.form-close-icon');
-const submit = document.querySelector('form-button');
+const submitButton = document.querySelector('.form-button');
+const confirmationPopup = document.querySelector('.confirmation-popup');
+const form = document.getElementById('myForm');
 
-
+// Mostrar o formulário e limpar os campos
 button.addEventListener('click', () => {
-    form.style.transform = "scale(1)";
+    formContainer.style.transform = "scale(1)";
     formPopUp.style.transform = "rotate(0)";
-    
+    clearFormFields();
 });
 
-overlay.addEventListener('click', () => {
-    form.style.transform = "scale(0)";
+// Fechar o formulário
+overlay.addEventListener('click', closeForm);
+xClose.addEventListener('click', closeForm);
+
+function closeForm() {
+    formContainer.style.transform = "scale(0)";
     formPopUp.style.transform = "rotate(540deg)";
+}
+
+// Enviar o formulário e mostrar o pop-up de confirmação
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault(); // Previne o envio do formulário para fins de demonstração
+
+    // Fechar o formulário
+    closeForm();
+
+    // Mostrar o pop-up de confirmação
+    confirmationPopup.style.display = 'block';
+    setTimeout(() => {
+        confirmationPopup.style.opacity = '1';
+    }, 200);
+
+    // Ocultar o pop-up de confirmação após 2 segundos
+    setTimeout(() => {
+        confirmationPopup.style.opacity = '0';
+        setTimeout(() => {
+            confirmationPopup.style.display = 'none';
+        }, 500); // Aguarda a transição de opacidade antes de ocultar
+    }, 2500);
+
+    clearFormFields();
 });
 
-xClose.addEventListener('click', () => {
-    form.style.transform = "scale(0)";
-    formPopUp.style.transform = "rotate(540deg)";
-});
-
+// Limpar os campos do formulário
+function clearFormFields() {
+    form.reset();
+}
--- src/header-menu-dropdown.js
+++ src/header-menu-dropdown.js
@@ -1,4 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Components
+    const menuBurger = document.querySelector('.header__menu-mobile');
+    const overlay = document.querySelector('.header__menu-mobile__overlay');
+    const dropdown = document.querySelector('.header__menu-mobile__dropdown');
+    const closeButton = document.querySelector('.close-button');
+
+
+    menuBurger.addEventListener('click', () => {
+
+        overlay.style.display = 'block';
+        dropdown.style.display = 'flex';
+
+        setTimeout(() => {
+            
+            overlay.style.opacity = '1';
+    
+            dropdown.style.transform = 'scaleX(1)';
+        }, 100);
+
+    
+    });
+
+    closeButton.addEventListener('click', () => {
+        closeMenu();
+    });
+
+    overlay.addEventListener('click', () => {
+        closeMenu();
+    });
+
+    function closeMenu() {
+
+        
+        overlay.style.opacity = '0';
+
+        dropdown.style.transform = 'scaleX(0)';
+
+        setTimeout(() => {
+            overlay.style.display = 'none';
+            dropdown.style.display = 'none';
+        }, 250)
+    }
 
 });
+
